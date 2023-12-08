b7f32cb4baf37eb5fa1d72741a7b66dfa275e0bc
MatthewAlbisser <Matthewalbisser@gmail.com>
2023-12-08 16:23:43 -0500

    Getting back  to this assignment.
    -Removed ColourChange script, was using unity physics to trigger. NOT ALLOWED.
    -Created new script for fresh start.
    -Easier colour change of objects ( Thank you Connor! ).
    -Had some work on object collider scripts, but reverted back after issues.
    -Added gitignore doc

--- Assets/Part 1 Scripts/ColourChange.cs
+++ Assets/Part 1 Scripts/ColourChange.cs
@@ -1,72 +1,91 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Text.RegularExpressions;
+//using UnityEngine;
 
-public class ColorChange : MonoBehaviour
-{
+//public class ColorChange : MonoBehaviour
+//{
 
-    public Color originalMaterial = Color.red; // Calls for RED
-    public Color customColor = Color.green;   // Calls GREEN for use 
-    private bool circleOverlaps = false;
-    private bool capsuleOverlaps = false;      // all 3 Bools the colourswitch, starts off (RED)
-    private bool squareOverlaps = false;
+//    public Color originalMaterial = Color.red; // Calls for RED
+//    public Color customColor = Color.green;   // Calls GREEN for use 
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.name == "Circle")
-        {
-            circleOverlaps = true;
-        }
-        else if (other.gameObject.name == "Capsule")  // Checks which object is colliding, trips bool
-        {
-            capsuleOverlaps = true;
-        }
-        else if (other.gameObject.name == "Square")
-        {
-            squareOverlaps = true;
-        }
+//    private bool circleOverlaps = false;
+//    private bool capsuleOverlaps = false;      // all 3 Bools the colour switch, starts off (RED)
+//    private bool squareOverlaps = false;
 
-        // Checks for all overlap variables
-        if ((circleOverlaps && capsuleOverlaps) || (circleOverlaps && squareOverlaps) || (capsuleOverlaps && squareOverlaps) || (circleOverlaps && capsuleOverlaps && squareOverlaps))
-        {
-            Debug.Log("colourswap");
-            TurnObjectsGreen();
-        }
-    }
+//    public GameObject circle;
+//    public GameObject square;
+//    public GameObject capsule;
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.name == "Circle")
-        {
-            circleOverlaps = false;
-        }
-        else if (other.gameObject.name == "Capsule")
-        {
-            capsuleOverlaps = false;
-        }
-        else if (other.gameObject.name == "Square")
-        {
-            squareOverlaps = false;
-        }
+//    private void OnTriggerEnter2D(Collider2D other)  // REMOVE
+//    {
+//        if (other.gameObject.name == "Circle")          // 
+//        {
+//            circleOverlaps = true;
+//        }
+//        else if (other.gameObject.name == "Capsule")    // Checks which objects are colliding, trips bool.
+//        {
+//            capsuleOverlaps = true;
+//        }
+//        else if (other.gameObject.name == "Square")
+//        {
+//            squareOverlaps = true;
+//        }                                               //
 
-        if (!circleOverlaps && !capsuleOverlaps && !squareOverlaps) // Checks for no overlap
-        {
-            ResetObjectsColor();
-        }
-    }
+//        // Checks for all overlap variables
+//        if ((circleOverlaps && capsuleOverlaps) || (circleOverlaps && squareOverlaps) || (capsuleOverlaps && squareOverlaps) || (circleOverlaps && capsuleOverlaps && squareOverlaps))
+//        {
+//            Debug.Log("colourswap");
+//            TurnObjectsGreen();
+//        }
+//    }
 
-    private void TurnObjectsGreen() // Change object colour to green
-    {
-        GameObject.Find("Circle").GetComponent<Renderer>().material.color = customColor;
-        GameObject.Find("Capsule").GetComponent<Renderer>().material.color = customColor;
-        GameObject.Find("Square").GetComponent<Renderer>().material.color = customColor;
-    }
+//    private void OnTriggerExit2D(Collider2D other) // REMOVE
+//    {
+//        if (other.gameObject.name == "Circle")
+//        {
+//            circleOverlaps = false;
+//        }
+//        else if (other.gameObject.name == "Capsule")        // Finds objects via name, 
+//        {
+//            capsuleOverlaps = false;
+//        }
+//        else if (other.gameObject.name == "Square")
+//        {
+//            squareOverlaps = false;
+//        }
+
+//        if (!circleOverlaps && !capsuleOverlaps && !squareOverlaps) // Checks for no overlap
+//        {
+//            ResetObjectsColor();
+//        }
+//    }
+
+//    private void TurnObjectsGreen() // Changes object colour to green
+//    {
+//        circle.GetComponent<Renderer>().material.color = customColor;
+//        capsule.GetComponent<Renderer>().material.color = customColor;
+//        square.GetComponent<Renderer>().material.color = customColor;
+//    }
 
 
-    private void ResetObjectsColor() // Reset objects to original colour 
-    {
-        GameObject.Find("Circle").GetComponent<Renderer>().material.color = originalMaterial; 
-        GameObject.Find("Capsule").GetComponent<Renderer>().material.color = originalMaterial; 
-        GameObject.Find("Square").GetComponent<Renderer>().material.color = originalMaterial; 
-    }
-}
+//    private void ResetObjectsColor() // Resets object to original colour 
+//    {
+//        circle.GetComponent<Renderer>().material.color = originalMaterial;
+//        capsule.GetComponent<Renderer>().material.color = originalMaterial;
+//        square.GetComponent<Renderer>().material.color = originalMaterial; 
+//    }
+//}
+////(capsuleOverlaps && squareOverlaps) || (circleOverlaps && capsuleOverlaps && squareOverlaps))
+
+
+//    //if (circleOverlaps && capsuleOverlaps)
+//    //{
+//    //        circle.GetComponent<Renderer>().material.color = customColor;
+//    //        capsule.GetComponent<Renderer>().material.color = customColor;
+//    //}
+
+//    //if (circleOverlaps && squareOverlaps)
+//    //if (capsuleOverlaps && squareOverlaps)
+//    //if (circleOverlaps && capsuleOverlaps && squareOverlaps)
+  
